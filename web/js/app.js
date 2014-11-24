5a4512cdc955186d497474bba527053219329423
Jerry Kilpatrick <jerry@zolan.org>
2014-11-23 22:40:47 -0800

    Very large update.
     Once again, this isn't even in a released state yet so my
     commit messages are still vague and pointless.
    
     This has a mostly working edit/view formatted recipe as well
     as a tiny touch to make it a little more pretty.

--- web/js/app.js
+++ web/js/app.js
@@ -5,13 +5,21 @@
         var self = this;
 
         this.refreshAllRecipes = function() {
-            $http.get('http://zolan.org:6622/recipes/all').success(function(data) {
-                $scope.allRecipes = data
-            })
+            $http.get('http://zolan.org:6622/recipes/all').success(function(retval) {
+                if(retval.status == 'success') {
+                    $scope.allRecipes = [null].concat(retval.data);
+                } else if(retval.status == 'failure') {
+                    console.log("Got an error!\n", retval.data);
+                } else {
+                    console.log("Got a weird state!!!");
+                }
+            }).error(function(data,status,headers,config) {
+                console.log("ERROR!", data,status,headers,config);
+            });
         };
 
         // Default allRecipes so there is at least something there prior to getting the data from the api.
-        $scope.allRecipes = [ {recipeID: 0, recipe_name: 'Loading...', recipe_raw: '', recipe_json: {} }];
+        $scope.allRecipes = [ null, {recipeID: 1, recipe_name: 'Loading...', recipe_raw: '', recipe_json: {} }];
 
         // Immediately grab all recipes.
         this.refreshAllRecipes();
--- web/js/recipe-display.js
+++ web/js/recipe-display.js
@@ -1,40 +1,137 @@
 (function(){
     app = angular.module('recipe-display', []);
 
-    app.controller('RecipeDisplayController', function($scope) {
+    app.controller('RecipeDisplayController', function($scope, $http) {
         var self = this;
 
+        self.formattedTabNum = 1;
+        self.rawTabNum       = 2;
+
         // Defaults
-        this.tab      = 2;
-        this.recipeID = 0;
+        self.tab      = 2;
+        self.recipeID = 1;
+        self.editing  = false;
 
-        this.selectTab  = function(tabNum) { self.tab = tabNum;          };
-        this.isSelected = function(tabNum) { return self.tab === tabNum; };
+        self.selectTab  = function(tabNum) { self.tab = tabNum;          };
+        self.isSelected = function(tabNum) { return self.tab === tabNum; };
 
-        this.showRawRecipe = function() {
-            return $scope.allRecipes[this.recipeID].recipe_raw;
+        self.recipe = function() {
+            return $scope.allRecipes[self.recipeID];
         };
 
-        this.showRecipeName = function() {
-            return $scope.allRecipes[this.recipeID].recipe_name;
+        self.hasFormattedRecipe = function() {
+            return self.recipeJSON().recipeSteps != null  && self.recipeJSON().recipeSteps.length > 0
+                ? true
+                : false;
         };
 
-        this.stepRecipe = function(step) {
+        self.rawRecipe = function() {
+            return self.recipe().recipe_raw;
+        };
 
-            var newID = this.recipeID + step;
+        self.recipeName = function() {
+            return self.recipe().recipe_name;
+        };
+
+        self.recipeJSON = function() {
+            return self.recipe().recipe_json || {};
+        };
+
+        self.stepRecipe = function(step) {
+
+            var newID = self.recipeID + step;
 
             if(newID >= $scope.allRecipes.length) {
-                newID = 0;
+                newID = 1;
             }
-            if(newID < 0) {
+            if(newID < 1) {
                 newID = $scope.allRecipes.length - 1;
             }
 
-            this.recipeID = newID;
+            self.recipeID = newID;
+            self.selectAppropriateInitialTab();
+
+            self.destroyEditForm();
+            self.editing = false;
         };
 
-        this.prevRecipe = function() { this.stepRecipe(-1); };
-        this.nextRecipe = function() { this.stepRecipe(1);  };
+        self.prevRecipe = function() { self.stepRecipe(-1); };
+        self.nextRecipe = function() { self.stepRecipe(1);  };
+
+        self.selectAppropriateInitialTab = function() {
+            if(self.hasFormattedRecipe()) {
+                console.log("FORMATTED");
+                self.tab = self.formattedTabNum;
+            } else {
+                self.tab = self.rawTabNum;
+            }
+        };
+
+        self.initializeEditForm = function() {
+            $http.get('http://zolan.org:6622/editFormOptions').success(function(retval) {
+                self.editFormOptions = retval.data
+
+                var recipeName     = self.recipeName();
+                var recipeJSON     = self.recipeJSON();
+                var recipeSkeleton = { recipeName: recipeName, recipeSteps: [], credit: '' };
+
+                var recipeStartVal = self.hasFormattedRecipe()
+                    ? recipeJSON
+                    : recipeSkeleton;
+
+                recipeStartVal = angular.copy(recipeStartVal);
+                self.editFormOptions.startval = recipeStartVal;
+
+                var element = document.getElementById('edit-form');
+                self.recipeEditor = new JSONEditor(element, self.editFormOptions);
+                self.recipeEditor.on('change', function() {
+                    var values = self.recipeEditor.getValue();
+                });
+            }).error(function(data,status,headers,config) {
+                console.log("ERROR!", data,status,headers,config);
+            });
+        };
+
+        self.destroyEditForm = function() {
+            if(self.recipeEditor != null) {
+                self.recipeEditor.destroy();
+            }
+        };
+
+        self.editRecipe = function() {
+            self.editing = !self.editing;
+
+            if(self.editing) {
+                self.initializeEditForm();
+            } else {
+                self.destroyEditForm();
+            }
+        };
+
+        self.updateRecipe = function() {
+            var recipeJSON = angular.copy(self.recipeEditor.getValue());
+            var recipeName = recipeJSON.recipeName;
+
+            var data = {
+                recipeName: recipeName,
+                recipeJSON: recipeJSON
+            };
+
+            $http.put('http://zolan.org:6622/recipes/' + self.recipeID, data).success(function(){ console.log("SUCCESS!") }).error(function() {
+                console.log("ERROR!", data,status,headers,config);
+            })
+        }
+
+        self.isEditing = function() {
+            return self.editing;
+        };
+    });
+
+    app.directive("editRecipe", function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'partials/edit-recipe.html'
+        };
     });
 
     app.directive("formattedRecipe", function() {
@@ -51,17 +148,10 @@
         };
     });
 
-    app.directive("navRecipeView", function() {
+    app.directive("navRecipe", function() {
         return {
             restrict: 'E',
-            templateUrl: 'partials/nav-recipe-view.html'
-        };
-    });
-
-    app.directive("navRecipePager", function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/nav-recipe-pager.html'
+            templateUrl: 'partials/nav-recipe.html'
         };
     });
 
